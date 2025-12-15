d26d65954fab83778de9817ff4d9e6c4241af381
Misael González Almeida <55672474+mga-97@users.noreply.github.com>
2025-12-15 15:21:05 +0100

    Revert "Initial scene generator module "

--- src/s2gos_generator/scene/__init__.py
+++
@@ -1,5 +0,0 @@
-from s2gos_utils.scene import SceneDescription
-
-from .config import create_s2gos_scene
-
-__all__ = ["SceneDescription", "create_s2gos_scene"]
--- src/s2gos_generator/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities for S2GOS scene generation."""
-
-from .geometry import create_aoi_polygon
-
-__all__ = ["create_aoi_polygon"]
